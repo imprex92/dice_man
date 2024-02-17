f3f6a9d43bf8809e8bb1fa55dca23c5b5df47e01
Daniel Bréelle Johannesson <56023250+imprex92@users.noreply.github.com>
2024-02-17 17:39:19 +0100

    building widgets, scaffold, appBar, styles

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,23 @@
 import 'package:flutter/material.dart';
 
+// void main() => runApp(const MaterialApp(home: Text('Hello My world!')));
 void main() {
-  runApp();
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blue[50],
+          titleTextStyle: TextStyle(
+              color: Colors.grey.shade800,
+              fontSize: 20,
+              fontWeight: FontWeight.w300),
+          title: const Center(child: Text('Rolle my Dice App')),
+        ),
+        backgroundColor: Colors.blue.shade400,
+        body: const Center(
+          child: Text('Hello My world!'),
+        ),
+      ),
+    ),
+  );
 }
